f2e4eeb0e0a97ee3733275344057b79b23b4d164
Myagis <zekonis.julius1@gmail.com>
2021-11-01 20:59:22 +0200

    add all workouts component

--- curlMaster/src/app/app.module.ts
+++ curlMaster/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AllWorkoutsComponent } from './components/all-workouts/all-workouts.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AllWorkoutsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [],
+  bootstrap: [AllWorkoutsComponent],
 })
 export class AppModule {}
